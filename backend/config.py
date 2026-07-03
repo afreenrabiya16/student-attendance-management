f0e7f66b18ea028b9cb8813c85265d10a7653306
afreenrabiya16 <afreenrabiya16@gmail.com>
2026-07-04 00:00:33 +0530

    Module 1: Initialize Flask backend with clean architecture

---
+++ backend/config.py
@@ -0,0 +1,8 @@
+class Config:
+    SECRET_KEY = "attendance_secret_key_2026"
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///instance/database.db"
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JWT_SECRET_KEY = "jwt_secret_key_2026"
